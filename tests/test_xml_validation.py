fcba4e15526c8e240268f1ed5861cee24a6e19c9
Scott Odle <sodle@splunk.com>
2026-08-02 00:56:14 -0600

    test: support connector Python baseline

--- tests/test_xml_validation.py
+++ tests/test_xml_validation.py
@@ -11,23 +11,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pytest
+import unittest
 
 from ivantiitsm_xml_validation import decode_xml_for_validation, reject_unsafe_xml_declarations
 
 
-@pytest.mark.parametrize("encoding", ["utf-8", "utf-8-sig", "utf-16", "utf-16-le", "utf-16-be", "utf-32", "utf-32-le", "utf-32-be"])
-def test_decode_xml_for_validation_recognizes_xml_encodings(encoding):
-    xml = "<?xml version='1.0'?><response/>"
-    assert decode_xml_for_validation(xml.encode(encoding)) == xml
+class XmlValidationTest(unittest.TestCase):
+    def test_recognizes_xml_encodings(self):
+        xml = "<?xml version='1.0'?><response/>"
+        encodings = ("utf-8", "utf-8-sig", "utf-16", "utf-16-le", "utf-16-be", "utf-32", "utf-32-le", "utf-32-be")
+        for encoding in encodings:
+            with self.subTest(encoding=encoding):
+                self.assertEqual(decode_xml_for_validation(xml.encode(encoding)), xml)
 
+    def test_rejects_encoded_dtds(self):
+        xml = "<?xml version='1.0'?><!DOCTYPE r [<!ENTITY a 'value'>]><r attr='&a;'/>"
+        encodings = ("utf-8", "utf-16", "utf-16-le", "utf-16-be", "utf-32", "utf-32-le", "utf-32-be")
+        for encoding in encodings:
+            with self.subTest(encoding=encoding), self.assertRaises(ValueError):
+                reject_unsafe_xml_declarations(xml.encode(encoding))
 
-@pytest.mark.parametrize("encoding", ["utf-8", "utf-16", "utf-16-le", "utf-16-be", "utf-32", "utf-32-le", "utf-32-be"])
-def test_reject_unsafe_xml_declarations_rejects_encoded_dtds(encoding):
-    xml = "<?xml version='1.0'?><!DOCTYPE r [<!ENTITY a 'value'>]><r attr='&a;'/>"
-    with pytest.raises(ValueError):
-        reject_unsafe_xml_declarations(xml.encode(encoding))
-
-
-def test_reject_unsafe_xml_declarations_accepts_plain_xml():
-    reject_unsafe_xml_declarations("<?xml version='1.0'?><response/>")
+    def test_accepts_plain_xml(self):
+        reject_unsafe_xml_declarations("<?xml version='1.0'?><response/>")
